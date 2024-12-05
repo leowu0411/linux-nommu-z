e223eaa515988d0c8256db927e9727049493952d
Hajime Tazaki <thehajime@gmail.com>
2024-12-05 19:46:47 +0900

    um: nommu: configure fs register on host syscall invocation
    As userspace on UML/!MMU also need to configure %fs register when it is
    running to correctly access thread structure, host syscalls implemented
    in os-Linux drivers may be puzzled when they are called.  Thus it has to
    configure %fs register via arch_prctl(SET_FS) on every host syscalls.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Ricardo Koller <ricarkol@google.com>

--- arch/um/os-Linux/process.c
+++ arch/um/os-Linux/process.c
@@ -16,6 +16,7 @@
 #include <sys/prctl.h>
 #include <sys/wait.h>
 #include <asm/unistd.h>
+#include <sys/syscall.h>   /* For SYS_xxx definitions */
 #include <linux/threads.h>
 #include <init.h>
 #include <longjmp.h>
@@ -176,6 +177,11 @@ out_close:
 	close(fd);
 out:
 	return ok;
+}
+
+int os_arch_prctl(int pid, int option, unsigned long *arg2)
+{
+	return syscall(SYS_arch_prctl, option, arg2);
 }
 
 void init_new_thread_signals(void)
--- arch/x86/um/nommu/do_syscall_64.c
+++ arch/x86/um/nommu/do_syscall_64.c
@@ -2,9 +2,37 @@
 
 #include <linux/kernel.h>
 #include <linux/ptrace.h>
+#include <asm/fsgsbase.h>
+#include <asm/prctl.h>
 #include <kern_util.h>
 #include <sysdep/syscalls.h>
 #include <os.h>
+
+static int os_x86_arch_prctl(int pid, int option, unsigned long *arg2)
+{
+	if (!host_has_fsgsbase)
+		return os_arch_prctl(pid, option, arg2);
+
+	switch (option) {
+	case ARCH_SET_FS:
+		wrfsbase(*arg2);
+		break;
+	case ARCH_SET_GS:
+		wrgsbase(*arg2);
+		break;
+	case ARCH_GET_FS:
+		*arg2 = rdfsbase();
+		break;
+	case ARCH_GET_GS:
+		*arg2 = rdgsbase();
+		break;
+	default:
+		pr_warn("%s: unsupported option: 0x%x", __func__, option);
+		break;
+	}
+
+	return 0;
+}
 
 __visible void do_syscall_64(struct pt_regs *regs)
 {
@@ -12,6 +40,9 @@ __visible void do_syscall_64(struct pt_regs *regs)
 
 	syscall = PT_SYSCALL_NR(regs->regs.gp);
 	UPT_SYSCALL_NR(&regs->regs) = syscall;
+
+	/* set fs register to the original host one */
+	os_x86_arch_prctl(0, ARCH_SET_FS, (void *)host_fs);
 
 	if (likely(syscall < NR_syscalls)) {
 		PT_REGS_SET_SYSCALL_RETURN(regs,
@@ -22,4 +53,10 @@ __visible void do_syscall_64(struct pt_regs *regs)
 
 	/* handle tasks and signals at the end */
 	interrupt_end();
+
+	/* restore back fs register to userspace configured one */
+	os_x86_arch_prctl(0, ARCH_SET_FS,
+		      (void *)(current->thread.regs.regs.gp[FS_BASE
+						     / sizeof(unsigned long)]));
+
 }
