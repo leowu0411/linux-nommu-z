bc4ce15362883cdf594cf34162bd67cd7d08de57
Hajime Tazaki <thehajime@gmail.com>
2024-12-07 20:01:28 +0900

    um: nommu: memory handling
    This commit adds memory operations on UML under !MMU environment.
    
    Some part of the original UML code relying on CONFIG_MMU are excluded
    from compilation when !CONFIG_MMU.  Additionally, generic functions such as
    uaccess, futex, memcpy/strnlen/strncpy can be used as user- and
    kernel-space share the address space in !CONFIG_MMU mode.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>
    Signed-off-by: Ricardo Koller <ricarkol@google.com>

--- arch/um/include/asm/uaccess.h
+++ arch/um/include/asm/uaccess.h
@@ -23,6 +23,7 @@
 #define __addr_range_nowrap(addr, size) \
 	((unsigned long) (addr) <= ((unsigned long) (addr) + (size)))
 
+#ifdef CONFIG_MMU
 extern unsigned long raw_copy_from_user(void *to, const void __user *from, unsigned long n);
 extern unsigned long raw_copy_to_user(void __user *to, const void *from, unsigned long n);
 extern unsigned long __clear_user(void __user *mem, unsigned long len);
@@ -34,9 +35,6 @@ static inline int __access_ok(const void __user *ptr, unsigned long size);
 
 #define INLINE_COPY_FROM_USER
 #define INLINE_COPY_TO_USER
-
-#include <asm-generic/uaccess.h>
-
 static inline int __access_ok(const void __user *ptr, unsigned long size)
 {
 	unsigned long addr = (unsigned long)ptr;
@@ -70,5 +68,8 @@ do {									\
 	barrier();							\
 	current->thread.segv_continue = NULL;				\
 } while (0)
+#endif
+
+#include <asm-generic/uaccess.h>
 
 #endif
