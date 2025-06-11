2679c4cc0bc3a4f41a223ea26c07d09bc6e59131
Jim Huang <jserv@ccns.ncku.edu.tw>
2025-06-12 02:15:56 +0800

    CI: Fix permission for using sysctl
    (cherry picked from commit 2cdf653ea58c9715b7523bfc034871babc1ffc03)

--- .github/workflows/netperf-bench.sh
+++ .github/workflows/netperf-bench.sh
@@ -5,8 +5,8 @@ TESTNAMES="TCP_STREAM TCP_MAERTS"
 DEST_ADDR=${1:-"192.168.122.1"}
 NETPERF=${2:-/root/netperf}
 
-/sbin/sysctl -w net.ipv4.tcp_wmem="40960 873800 1677721600"
-/sbin/sysctl -w net.ipv4.tcp_rmem="40960 873800 1677721600"
+sudo /sbin/sysctl -w net.ipv4.tcp_wmem="40960 873800 1677721600"
+sudo /sbin/sysctl -w net.ipv4.tcp_rmem="40960 873800 1677721600"
 
 for size in $PSIZES
 do
