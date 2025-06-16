c2aedfa54c4441abee021f43fbc4a7c7ddfef895
Hajime Tazaki <thehajime@gmail.com>
2025-06-17 08:34:10 +0900

    um: nommu: a work around for MMU dependency to PCI driver
    The commit 8fe743b5eba0 ("PCI: Add CONFIG_MMU dependency") restricts the
    PCI base driver to depend on MMU.  While nommu UML _can_ implement PCI
    drivers over PCI devices (e.g., virtio-pci), the current nommu UML
    doesn't implement it.
    
    But without PCI drivers kunit complains as config for kunit
    (arch_uml.config) defines the dependency to PCI drivers.
    
    This commit fixes the issue of this compile failures when building PCI
    drivers with nommu UML.  In particular, the fix is to undefine the
    constant PCI_IOBASE to be able to bypass pci_unmap_iospace() call.
    
    When we will support PCI drivers for nommu UML, we will refactor this
    code.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- arch/um/include/asm/dma.h
+++ arch/um/include/asm/dma.h
@@ -4,6 +4,19 @@
 
 #include <asm/io.h>
 
+/**
+ * now the PCI core driver depends on CONFIG_MMU in linus tree, nommu
+ * UML cannot build with PCI but without PCI kunit doesn't build due
+ * to the dependency to the CONFIG_VIRTIO_UML.
+ *
+ * This is a workaround to silence build failures on kunit, which is
+ * valid until nommu UML supports PCI drivers (e.g., virtio-pci) in a
+ * future.
+ */
+#ifndef CONFIG_MMU
+#undef PCI_IOBASE
+#endif
+
 extern unsigned long uml_physmem;
 
 #define MAX_DMA_ADDRESS (uml_physmem)
